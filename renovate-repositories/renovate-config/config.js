bcff05b96d1156d186e6cd603a7d4ce009721573
Axel Böswetter <boeswetter@portrino.de>
2025-03-24 13:22:31 +0100

    * import Renovate slides and config

---
+++ renovate-repositories/renovate-config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "allowedEnv": [
+        "COMPOSER_*",
+    ],
+    "autodiscover": true,
+    "detectHostRulesFromEnv": true,
+    "endpoint": "https://git.portrino.de/api/v1",
+    "gitAuthor": "Renovate Bot <renovate@portrino.com>",
+    "onboardingConfigFileName": "renovate.json",
+    "optimizeForDisabled": true,
+    "platform": "gitea"
+};
